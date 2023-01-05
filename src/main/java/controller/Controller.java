ba7b0a8de43330d682300f8485d9645ce25494c4
Maxym Perebiynis <maksperebiynos983@gmail.com>
2023-01-05 22:08:54 +0200

    controller fix

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,34 +2,61 @@ package controller;
 
 import command.Command;
 import command.CommandFactory;
+import command.CommandType;
 import command.page.PageUrl;
+import entity.enums.UserRole;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Serial;
 
-public class Controller {
-    public static void process(HttpServletRequest request, HttpServletResponse response) {
+@WebServlet(urlPatterns = {"/login", "/app", "/admin", "/driver", "/client"})
+public class Controller extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        process(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        process(request, response);
+    }
+
+    public void process(HttpServletRequest request, HttpServletResponse response) {
         try {
             CommandFactory factory = new CommandFactory();
             Command command = factory.getCommand(request);
-            PageUrl page = command.execute(request);
-            if (page.isRedirection()) redirect(page, request, response);
-            else forward(page, request, response);
+            PageUrl page = null;
+            if (command == CommandType.getCommand("registration")) {
+                page = command.execute(request);
+                if (UserRole.valueOf(request.getParameter("role")) == UserRole.client) {
+                    command = CommandType.getCommand("registrationClient");
+                } else
+                    command = CommandType.getCommand("registrationDriver");
+            }
+            page = command.execute(request);
+            if (page.isRedirection())
+                redirect(page, request, response);
+            else
+                forward(page, request, response);
         } catch (ServletException | IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void redirect(PageUrl page, HttpServletRequest request, HttpServletResponse response)
+    private void redirect(PageUrl page, HttpServletRequest request, HttpServletResponse response)
             throws IOException {
         String url = page.getUrlPath();
         response.sendRedirect(request.getContextPath() + url);
     }
 
-    private static void forward(PageUrl page, HttpServletRequest request, HttpServletResponse response)
+    private void forward(PageUrl page, HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String message = page.getMessage();
         if (message != null)
