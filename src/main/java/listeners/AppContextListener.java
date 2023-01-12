f4a2165427491ecd75028e379f7f8680431c124a
Maxym Perebiynis <maksperebiynos983@gmail.com>
2023-01-12 20:18:27 +0200

    appcontext listener, invalidate all sessions when app is closing

---
+++ src/main/java/listeners/AppContextListener.java
@@ -0,0 +1,34 @@
+package listeners;
+
+import entity.Order;
+import pool.ConnectionPool;
+import service.DriverService;
+import service.OrderService;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext ctx = servletContextEvent.getServletContext();
+    }
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        ServletContext ctx = servletContextEvent.getServletContext();
+        HashMap<String, HttpSession> userSessions =
+                (HashMap<String, HttpSession>) ctx.getAttribute("userSessions");
+        if (userSessions != null){
+            System.out.println("session invalidated");
+            userSessions.forEach((k,v)-> v.invalidate());
+        }
+        ctx.removeAttribute("userSessions");
+    }
+
+}
+
